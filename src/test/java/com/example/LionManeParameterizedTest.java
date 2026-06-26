426cd64a0a70e1e59ba70b113bd99c7a65b6efe4
gordey97 <sgordej1402@gmail.com>
2026-06-26 11:13:44 +0300

    Update LionManeParameterizedTest.java

--- src/test/java/com/example/LionManeParameterizedTest.java
+++ src/test/java/com/example/LionManeParameterizedTest.java
@@ -18,7 +18,7 @@ public class LionManeParameterizedTest {
         this.hasMane = hasMane;
     }
 
-    @Parameterized.Parameters
+    @Parameterized.Parameters(name = "Тестовые данные: {0}, {1}")
     public static Object[][] data() {
         return new Object[][]{
                 {"Самец", true},
@@ -29,6 +29,7 @@ public class LionManeParameterizedTest {
     @Test
     public void doesHaveManeReturnsExpectedValue() throws Exception {
         Lion lion = new Lion(sex, mock(Feline.class));
+
         assertEquals(hasMane, lion.doesHaveMane());
     }
 }
